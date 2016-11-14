83914b5829711c0b521cbd5e18307bb1cf4adaa7
Jacob Weber <hello@jacobweber.net>
2016-11-14 10:59:28 -0600

    Update homebrew-cask.sh

--- src/os/install/macos/homebrew-cask.sh
+++ src/os/install/macos/homebrew-cask.sh
@@ -15,14 +15,10 @@ install_homebrew_cask() {
     "Alfred"
   cask_install "atom" \
     "Atom.io"
-  cask_install "bartender" \
-    "Bartender"
   cask_install "bettertouchtool" \
     "Better Touch Tool"
   cask_install "clipmenu" \
     "ClipMenu"
-  cask_install "crashplan" \
-    "CrashPlan Backups"
   cask_install "daisydisk" \
     "DaisyDisk"
   cask_install "docker" \
@@ -37,58 +33,36 @@ install_homebrew_cask() {
     "Firefox"
   cask_install "font-fira-code" \
     "Fira Code Font"
-  cask_install "garmin-express" \
-    "Garmin Express"
   cask_install "grammarly" \
     "Grammarly"
   cask_install "google-chrome" \
     "Google Chrome"
-  cask_install "handbrake" \
-    "HandBrake"
   cask_install "font-hack" \
     "Hack Font"
   cask_install "iterm2" \
     "iTerm2"
-  cask_install "kindle" \
-    "Kindle"
   cask_install "logitech-control-center" \
     "Logitech Control Center"
   cask_install "microsoft-office" \
     "Microsoft Office"
-  cask_install "minecraft" \
-    "Minecraft"
-  cask_install "moom" \
-    "Moom"
   cask_install "ngrok" \
     "Ngrok"
-  cask_install "phpstorm-eap" \
-    "PHPStorm EAP"
-  cask_install "plex-home-theater" \
-    "Plex Home Theater"
   cask_install "sequel-pro" \
     "Sequal Pro"
   cask_install "skype" \
     "Skype"
   cask_install "slack" \
     "Slack"
-  cask_install "sonos" \
-    "Sonos"
   cask_install "spotify" \
     "Spotify"
-  cask_install "steam" \
-    "Steam"
   cask_install "sublime-text3" \
     "Sublime Text 3"
   cask_install "superduper" \
     "Superduper!"
   cask_install "teamviewer" \
     "Team Viewer"
-  cask_install "transmission" \
-    "Transmission"
   cask_install "transmit" \
     "Transmit"
-  cask_install "tunnelbear" \
-    "TunnelBear"
   cask_install "vlc" \
     "VLC"
 }
